f434539ded96424ad43ab5dfe56c400a68fc6edf
Laura Mrtnz <lauramsolana93@gmail.com>
2020-05-07 13:30:32 +0200

    [FIX] Init Single<LocalInputData> has problems do init

--- app/src/main/java/com/kotlin/seat/seatmars/model/MarsLocalData.kt
+++ app/src/main/java/com/kotlin/seat/seatmars/model/MarsLocalData.kt
@@ -2,6 +2,7 @@ package com.kotlin.seat.seatmars.model
 
 import android.content.Context
 import com.google.gson.Gson
+import com.kotlin.seat.seatmars.common.data.local.LocalAxis
 import com.kotlin.seat.seatmars.common.data.local.LocalInputData
 import com.kotlin.seat.seatmars.common.utils.JsonParser
 import io.reactivex.Single
@@ -15,6 +16,14 @@ class MarsLocalData : MarsDataContract.Local {
     override fun getDataFromJson(context: Context): Single<LocalInputData> {
         jsonParser = JsonParser()
         val jsonFileString = jsonParser.getJsonDataFromAssets(context, "input.json")
+        localInputData = Single.just(
+            LocalInputData(
+                topRightCorner = LocalAxis(0, 0),
+                roverPosition = LocalAxis(0, 0),
+                roverDirection = 'N',
+                movements = ""
+            )
+        )
 
         return localInputData.map {
             gson.fromJson(jsonFileString, LocalInputData::class.java)
