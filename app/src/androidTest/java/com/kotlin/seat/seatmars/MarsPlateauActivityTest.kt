7cb42e7246996bd584eb06f8c8d956783a395319
Laura Mrtnz <lauramsolana93@gmail.com>
2020-05-07 13:29:34 +0200

    [ADD] Barista Library for UI Tests

--- app/src/androidTest/java/com/kotlin/seat/seatmars/MarsPlateauActivityTest.kt
+++ app/src/androidTest/java/com/kotlin/seat/seatmars/MarsPlateauActivityTest.kt
@@ -1,20 +1,22 @@
 package com.kotlin.seat.seatmars
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import androidx.test.rule.ActivityTestRule
+import androidx.test.platform.app.InstrumentationRegistry
 import com.facebook.testing.screenshot.Screenshot
+import com.facebook.testing.screenshot.ViewHelpers
 import com.kotlin.seat.seatmars.tests.InputDataFakeObject
 import com.kotlin.seat.seatmars.ui.activity.MarsPlateauActivity
 import com.microsoft.appcenter.espresso.Factory
 import com.microsoft.appcenter.espresso.ReportHelper
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotExist
+import com.schibsted.spain.barista.rule.BaristaRule
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -26,10 +28,15 @@ class MarsPlateauActivityTest {
     lateinit var mLayoutInflater: LayoutInflater
     lateinit var view: View
 
+    val context: Context by lazy {
+        InstrumentationRegistry.getInstrumentation().targetContext
+    }
+
 
     @Rule
     @JvmField
-    var mActivityTestRule = ActivityTestRule(MarsPlateauActivity::class.java)
+    var mActivityTestRule : BaristaRule<MarsPlateauActivity> =
+        BaristaRule.create(MarsPlateauActivity::class.java)
 
 
     @Rule
@@ -38,55 +45,59 @@ class MarsPlateauActivityTest {
 
     @Before
     fun init() {
-        mActivityTestRule.activity
-        /*mLayoutInflater = LayoutInflater.from(mActivityTestRule.activity)
+        mLayoutInflater = LayoutInflater.from(context)
         view = mLayoutInflater.inflate(R.layout.activity_main, null, false)
-        ViewHelpers.setupView(view).setExactWidthDp(360).layout()*/
+        ViewHelpers.setupView(view).setExactWidthDp(360).layout()
 
     }
 
     @Test
     fun checkViewOnEnter() {
+        mActivityTestRule.launchActivity()
         reportHelper.label("MARS PLATEAU ACTIVITY ON ENTER")
-        onView(withId(R.id.start_button)).check(matches(isDisplayed()))
+        assertDisplayed(R.id.start_button)
         Screenshot.snap(view).record()
     }
 
     @Test
     fun checkTableDisplayed() {
+        mActivityTestRule.launchActivity()
         reportHelper.label("MARS PLATEAU ACTIVITY ON CLICK START BUTTON")
+        assertDisplayed(R.id.start_button)
         onView(withId(R.id.start_button)).perform(click())
-        onView(withId(R.id.table_plateau)).check(matches(isDisplayed()))
-        onView(withId(R.id.move_button)).check(matches(isDisplayed()))
+        assertDisplayed(R.id.table_plateau)
+        assertDisplayed(R.id.move_button)
         Screenshot.snap(view).record()
     }
 
     @Test
     fun checkTableUpdate() {
+        mActivityTestRule.launchActivity()
         reportHelper.label("MARS PLATEAU ACTIVITY ON UPDATE TABLE")
-        onView(withId(R.id.start_button)).check(matches(isDisplayed()))
+        assertDisplayed(R.id.start_button)
         onView(withId(R.id.start_button)).perform(click())
-        onView(withId(R.id.move_button)).check(matches(isDisplayed()))
+        assertDisplayed(R.id.move_button)
         onView(withId(R.id.move_button)).perform(click())
-        onView(withId(R.id.table_plateau)).check(matches(isDisplayed()))
+        assertDisplayed(R.id.table_plateau)
         Screenshot.snap(view).record()
     }
 
     @Test
     fun checkFragmentDisplayed() {
+        mActivityTestRule.launchActivity()
         reportHelper.label("MARS PLATEAU ACTIVITY ON LAST POSITION")
-        onView(withId(R.id.start_button)).check(matches(isDisplayed()))
+        assertDisplayed(R.id.start_button)
         onView(withId(R.id.start_button)).perform(click())
         var inputData = InputDataFakeObject.getLocalInputData()
         var movesList = inputData.movements.chunked(1)
 
         for (i in movesList.indices) {
-            onView(withId(R.id.move_button)).check(matches(isDisplayed()))
+            assertDisplayed(R.id.move_button)
             onView(withId(R.id.move_button)).perform(click())
             if (i == movesList.size - 1) {
-                onView(withId(R.id.move_button)).check(doesNotExist())
-                mActivityTestRule.activity.supportFragmentManager.findFragmentByTag("LAST MOVE")
-                onView(withId(R.id.message)).check(matches(isDisplayed()))
+                assertNotExist(R.id.move_button)
+                mActivityTestRule.activityTestRule.activity.supportFragmentManager.findFragmentByTag("LAST MOVE")
+                assertDisplayed(R.id.message)
             }
         }
 
